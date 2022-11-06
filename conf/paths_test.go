de9e47ec6c0d1f78db8e0bdfa0bc585fbdd8d674
Skip Baney <twelvelabs@gmail.com>
2022-11-05 23:23:56 -0500

    fix test failures in gh

--- conf/paths_test.go
+++ conf/paths_test.go
@@ -1,8 +1,6 @@
 package conf
 
 import (
-	"fmt"
-	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
@@ -11,112 +9,199 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConfigDir_WhenWindows(t *testing.T) {
-	stubs := gostub.StubFunc(&isWindowsFunc, true)
-	defer stubs.Reset()
+func TestConfigDir(t *testing.T) {
+	tests := []struct {
+		desc            string
+		HOME            string
+		AppData         string
+		XDG_CONFIG_HOME string
+		windows         bool
+		expected        string
+	}{
+		{
+			desc:            "[non-windows] default",
+			HOME:            "HOME_DIR",
+			AppData:         "APP_DATA_DIR",
+			XDG_CONFIG_HOME: "",
+			windows:         false,
+			expected:        filepath.Join("HOME_DIR", ".config", "my-app"),
+		},
+		{
+			desc:            "[non-windows] xdg",
+			HOME:            "HOME_DIR",
+			AppData:         "APP_DATA_DIR",
+			XDG_CONFIG_HOME: "XDG_DIR",
+			windows:         false,
+			expected:        filepath.Join("XDG_DIR", "my-app"),
+		},
 
-	// default is ~/.config/$name
-	home, _ := os.UserHomeDir()
-	assert.Equal(t, filepath.Join(home, ".config", "my-app"), ConfigDir("my-app"))
-
-	// but prefers $AppData/$name
-	t.Setenv(appData, "DATA_DIR")
-
-	fmt.Println("")
-	fmt.Println(" ==> os.Environ")
-	for _, line := range os.Environ() {
-		fmt.Println(line)
+		{
+			desc:            "[windows] default",
+			HOME:            "HOME_DIR",
+			AppData:         "",
+			XDG_CONFIG_HOME: "",
+			windows:         true,
+			expected:        filepath.Join("HOME_DIR", ".config", "my-app"),
+		},
+		{
+			desc:            "[windows] app data",
+			HOME:            "HOME_DIR",
+			AppData:         "APP_DATA_DIR",
+			XDG_CONFIG_HOME: "",
+			windows:         true,
+			expected:        filepath.Join("APP_DATA_DIR", "my-app"),
+		},
+		{
+			desc:            "[windows] xdg",
+			HOME:            "HOME_DIR",
+			AppData:         "APP_DATA_DIR",
+			XDG_CONFIG_HOME: "XDG_DIR",
+			windows:         true,
+			expected:        filepath.Join("XDG_DIR", "my-app"),
+		},
 	}
-	fmt.Println("")
-	fmt.Println("")
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			stubs := gostub.StubFunc(&isWindowsFunc, tt.windows)
+			defer stubs.Reset()
 
-	assert.Equal(t, true, isWindowsFunc())
-	assert.Equal(t, "DATA_DIR", os.Getenv(appData))
+			t.Setenv("HOME", tt.HOME)
+			t.Setenv("AppData", tt.AppData)
+			t.Setenv("XDG_CONFIG_HOME", tt.XDG_CONFIG_HOME)
 
-	assert.Equal(t, filepath.Join("DATA_DIR", "my-app"), ConfigDir("my-app"))
-
-	// XDG dir takes precedence over the above.
-	t.Setenv(xdgConfigHome, "XDG_DIR")
-	assert.Equal(t, filepath.Join("XDG_DIR", "my-app"), ConfigDir("my-app"))
-}
-
-func TestConfigDir_WhenNotWindows(t *testing.T) {
-	stubs := gostub.StubFunc(&isWindowsFunc, false)
-	defer stubs.Reset()
-
-	t.Setenv(appData, "DATA_DIR")
-
-	// default is ~/.config/$name (regardless of $AppData)
-	home, _ := os.UserHomeDir()
-	assert.Equal(t, filepath.Join(home, ".config", "my-app"), ConfigDir("my-app"))
-
-	// XDG dir takes precedence over the above.
-	t.Setenv(xdgConfigHome, "XDG_DIR")
-	assert.Equal(t, filepath.Join("XDG_DIR", "my-app"), ConfigDir("my-app"))
+			assert.Equal(t, tt.expected, ConfigDir("my-app"))
+		})
+	}
 }
 
 func TestStateDir_WhenWindows(t *testing.T) {
-	stubs := gostub.StubFunc(&isWindowsFunc, true)
-	defer stubs.Reset()
+	tests := []struct {
+		desc           string
+		HOME           string
+		LocalAppData   string
+		XDG_STATE_HOME string
+		windows        bool
+		expected       string
+	}{
+		{
+			desc:           "[non-windows] default",
+			HOME:           "HOME_DIR",
+			LocalAppData:   "APP_DATA_DIR",
+			XDG_STATE_HOME: "",
+			windows:        false,
+			expected:       filepath.Join("HOME_DIR", ".local", "state", "my-app"),
+		},
+		{
+			desc:           "[non-windows] xdg",
+			HOME:           "HOME_DIR",
+			LocalAppData:   "APP_DATA_DIR",
+			XDG_STATE_HOME: "XDG_DIR",
+			windows:        false,
+			expected:       filepath.Join("XDG_DIR", "my-app"),
+		},
 
-	// default is ~/.local/state/$name
-	home, _ := os.UserHomeDir()
-	assert.Equal(t, filepath.Join(home, ".local", "state", "my-app"), StateDir("my-app"))
+		{
+			desc:           "[windows] default",
+			HOME:           "HOME_DIR",
+			LocalAppData:   "",
+			XDG_STATE_HOME: "",
+			windows:        true,
+			expected:       filepath.Join("HOME_DIR", ".local", "state", "my-app"),
+		},
+		{
+			desc:           "[windows] app data",
+			HOME:           "HOME_DIR",
+			LocalAppData:   "APP_DATA_DIR",
+			XDG_STATE_HOME: "",
+			windows:        true,
+			expected:       filepath.Join("APP_DATA_DIR", "my-app"),
+		},
+		{
+			desc:           "[windows] xdg",
+			HOME:           "HOME_DIR",
+			LocalAppData:   "APP_DATA_DIR",
+			XDG_STATE_HOME: "XDG_DIR",
+			windows:        true,
+			expected:       filepath.Join("XDG_DIR", "my-app"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			stubs := gostub.StubFunc(&isWindowsFunc, tt.windows)
+			defer stubs.Reset()
 
-	// but prefers $LocalAppData/$name
-	t.Setenv(localAppData, "DATA_DIR")
-	assert.Equal(t, filepath.Join("DATA_DIR", "my-app"), StateDir("my-app"))
+			t.Setenv("HOME", tt.HOME)
+			t.Setenv("LocalAppData", tt.LocalAppData)
+			t.Setenv("XDG_STATE_HOME", tt.XDG_STATE_HOME)
 
-	// XDG dir takes precedence over the above.
-	t.Setenv(xdgStateHome, "XDG_DIR")
-	assert.Equal(t, filepath.Join("XDG_DIR", "my-app"), StateDir("my-app"))
-}
-
-func TestStateDir_WhenNotWindows(t *testing.T) {
-	stubs := gostub.StubFunc(&isWindowsFunc, false)
-	defer stubs.Reset()
-
-	t.Setenv(localAppData, "DATA_DIR")
-
-	// default is ~/.local/state/$name
-	home, _ := os.UserHomeDir()
-	assert.Equal(t, filepath.Join(home, ".local", "state", "my-app"), StateDir("my-app"))
-
-	// XDG dir takes precedence over the above.
-	t.Setenv(xdgStateHome, "XDG_DIR")
-	assert.Equal(t, filepath.Join("XDG_DIR", "my-app"), StateDir("my-app"))
+			assert.Equal(t, tt.expected, StateDir("my-app"))
+		})
+	}
 }
 
 func TestDataDir_WhenWindows(t *testing.T) {
-	stubs := gostub.StubFunc(&isWindowsFunc, true)
-	defer stubs.Reset()
+	tests := []struct {
+		desc          string
+		HOME          string
+		LocalAppData  string
+		XDG_DATA_HOME string
+		windows       bool
+		expected      string
+	}{
+		{
+			desc:          "[non-windows] default",
+			HOME:          "HOME_DIR",
+			LocalAppData:  "APP_DATA_DIR",
+			XDG_DATA_HOME: "",
+			windows:       false,
+			expected:      filepath.Join("HOME_DIR", ".local", "share", "my-app"),
+		},
+		{
+			desc:          "[non-windows] xdg",
+			HOME:          "HOME_DIR",
+			LocalAppData:  "APP_DATA_DIR",
+			XDG_DATA_HOME: "XDG_DIR",
+			windows:       false,
+			expected:      filepath.Join("XDG_DIR", "my-app"),
+		},
 
-	// default is ~/.local/state/$name
-	home, _ := os.UserHomeDir()
-	assert.Equal(t, filepath.Join(home, ".local", "share", "my-app"), DataDir("my-app"))
+		{
+			desc:          "[windows] default",
+			HOME:          "HOME_DIR",
+			LocalAppData:  "",
+			XDG_DATA_HOME: "",
+			windows:       true,
+			expected:      filepath.Join("HOME_DIR", ".local", "share", "my-app"),
+		},
+		{
+			desc:          "[windows] app data",
+			HOME:          "HOME_DIR",
+			LocalAppData:  "APP_DATA_DIR",
+			XDG_DATA_HOME: "",
+			windows:       true,
+			expected:      filepath.Join("APP_DATA_DIR", "my-app"),
+		},
+		{
+			desc:          "[windows] xdg",
+			HOME:          "HOME_DIR",
+			LocalAppData:  "APP_DATA_DIR",
+			XDG_DATA_HOME: "XDG_DIR",
+			windows:       true,
+			expected:      filepath.Join("XDG_DIR", "my-app"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			stubs := gostub.StubFunc(&isWindowsFunc, tt.windows)
+			defer stubs.Reset()
 
-	// but prefers $LocalAppData/$name
-	t.Setenv(localAppData, "DATA_DIR")
-	assert.Equal(t, filepath.Join("DATA_DIR", "my-app"), DataDir("my-app"))
+			t.Setenv("HOME", tt.HOME)
+			t.Setenv("LocalAppData", tt.LocalAppData)
+			t.Setenv("XDG_DATA_HOME", tt.XDG_DATA_HOME)
 
-	// XDG dir takes precedence over the above.
-	t.Setenv(xdgDataHome, "XDG_DIR")
-	assert.Equal(t, filepath.Join("XDG_DIR", "my-app"), DataDir("my-app"))
-}
-
-func TestDataDir_WhenNotWindows(t *testing.T) {
-	stubs := gostub.StubFunc(&isWindowsFunc, false)
-	defer stubs.Reset()
-
-	t.Setenv(localAppData, "DATA_DIR")
-
-	// default is ~/.local/state/$name
-	home, _ := os.UserHomeDir()
-	assert.Equal(t, filepath.Join(home, ".local", "share", "my-app"), DataDir("my-app"))
-
-	// XDG dir takes precedence over the above.
-	t.Setenv(xdgDataHome, "XDG_DIR")
-	assert.Equal(t, filepath.Join("XDG_DIR", "my-app"), DataDir("my-app"))
+			assert.Equal(t, tt.expected, DataDir("my-app"))
+		})
+	}
 }
 
 func TestIsWindows(t *testing.T) {
