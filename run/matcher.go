63d7b725e317d391d78fdca28b0f3d4ec3f3700f
Skip Baney <twelvelabs@gmail.com>
2022-12-18 14:10:42 -0600

    feat: Add run.MatchStdin stub matcher (#20)

--- run/matcher.go
+++ run/matcher.go
@@ -1,7 +1,13 @@
 package run
 
 import (
+	"io"
 	"regexp"
+)
+
+var (
+	// for stubbing
+	ioReadAll = io.ReadAll
 )
 
 // Matcher is function that matches commands.
@@ -10,6 +16,32 @@ type Matcher func(cmd *Cmd) bool
 // MatchAny is a matcher that matches any command.
 func MatchAny(cmd *Cmd) bool {
 	return true
+}
+
+// MatchAll returns a matcher that returns true if all matcher args match.
+func MatchAll(matchers ...Matcher) Matcher {
+	return func(cmd *Cmd) bool {
+		for _, matcher := range matchers {
+			if ok := matcher(cmd); !ok {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// MatchStdin returns a matcher that matches against command stdin.
+func MatchStdin(s string) Matcher {
+	return func(cmd *Cmd) bool {
+		if cmd.Stdin == nil {
+			return false
+		}
+		buf, err := ioReadAll(cmd.Stdin)
+		if err != nil {
+			panic(err)
+		}
+		return s == string(buf)
+	}
 }
 
 // MatchString returns a matcher that matches against command strings.
