89ba1aece92695766eab461e8d3ccbeed0e0c789
Skip Baney <twelvelabs@gmail.com>
2022-12-26 16:52:52 -0600

    fix: ExitError should be a wrapper around std lib version

--- run/errors.go
+++ run/errors.go
@@ -2,22 +2,30 @@ package run
 
 import (
 	"fmt"
+	"os/exec"
 )
 
 // NewExitError returns a new exit error for code.
 func NewExitError(code int) *ExitError {
-	return &ExitError{code: code}
+	return &ExitError{
+		ExitError: &exec.ExitError{},
+		code:      code,
+	}
 }
 
-// ExitError is a replacement for [exec.ExitError] used when stubbing that
-// allows for accessing the exit code.
+// ExitError is a light wrapper for [exec.ExitError] that allows for easily accessing
+// and stubbing the exit code.
 //
 // The reason we don't use [exec.ExitError] directly is that it stores the
 // exit code inside `os.ProcessState`, which in turn uses `syscall.WaitStatus`,
 // and WaitStatus is implemented differently per-system.
 type ExitError struct {
-	Stderr []byte
-	code   int
+	*exec.ExitError
+	code int
+}
+
+func (e *ExitError) Unwrap() error {
+	return e.ExitError
 }
 
 // Code returns the exit code.
